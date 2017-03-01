f436c75f9b240f38f70a36af0ca99086a4575839
VInicius <viniciuspuente_2@hotmail.com>
2017-03-01 13:11:02 -0300

    Gulpfile added.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+sourcemaps = require('gulp-sourcemaps'),
+browserSync = require('browser-sync').create();
+
+
+var config = {
+  srcPath: = 'src/',
+  distPath:'styles/'
+};
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+    port: 8080,
+    startPath: 'index.html',
+  })
+});
+
+gulp.task('sass', function(){
+  return gulp.src(config.srcPath+'sass/**/*.+(scss|sass)')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError)) // Using gulp-sass
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.distPath+'css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
